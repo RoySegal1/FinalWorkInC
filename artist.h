5b477b2ad3be1b14477f00cb8b2a5a5a07977ca3
eilonaharoni <eaharoni88@gmail.com>
2024-03-31 13:44:49 +0300

    Merge remote-tracking branch 'origin/master' into Eilon_Branch

--- artist.h
+++ artist.h
@@ -12,7 +12,7 @@ typedef struct{
 
 void showArtist(Artist* artist);
 void initArtist(Artist* artist);
-void getArtistName(Artist* artist);
+//void getArtistName(Artist* artist);
 int	loadArtistFromTextFile(Artist * artist, FILE* fp);
 int	saveArtistToTextFile(const Artist * artist, FILE* fp);
 void creatArtist(Artist* artist);
