71bc23aa33522838c410911c9cb7cccd82487368
roy <segalroi22@gmail.com>
2024-03-31 13:26:17 +0300

    Checked Song Binary and Text

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "stdio.h"
 #include "artist.h"
-
+#include "song.h"
 
 int main() {
-
+	/*Artist A;
+	initArtist(&A);
+	Song s;
+	initSong(&s,&A);
+	FILE* fp;
+	fp = fopen("Song.txt", "w");
+	if (!fp)
+		return 0;
+	writeSongToTextFile(&s, fp);
+	fclose(fp);*/
+	FILE* fp;
+	fp = fopen("Song.bin", "rb");
+	//Artist A;
+	//initArtist(&A);
+	Song s;
+	//initSong(&s, &A);
+	readSongFromBFile(&s, fp);
+	printSong(&s);
+	fclose(fp);
 	return 0;
 }
