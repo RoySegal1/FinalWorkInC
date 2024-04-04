f493f87deb12b33e2c998d7e59166717df6d049a
eilonaharoni <eaharoni88@gmail.com>
2024-04-04 12:51:02 +0300

    Merge remote-tracking branch 'origin/master' into Eilon_Branch

--- artist.c
+++ artist.c
@@ -4,6 +4,7 @@
 #include "string.h"
 #include "General.h"
 #include "fileHelper.h"
+#include "macros.h"
 
 
 void initArtist(Artist* artist)
@@ -51,17 +52,29 @@ int	saveArtistToTextFile(const Artist * artist, FILE* fp)
 int	loadArtistFromTextFile(Artist * artist, FILE* fp)
 {
     char temp[MAX_STR_LEN];
-    if (!artist)
-        return 0;
+    CHECK_RETURN_0(artist)
 
     myGets(temp, MAX_STR_LEN, fp);
     artist->name = getDynStr(temp);
+    CHECK_RETURN_0_PRINT(artist->name,"Error reading artist name\n")
 
     myGets(temp, MAX_STR_LEN, fp);
     artist->aboutMe = getDynStr(temp);
-
-    fscanf(fp, "%d", &artist->amountOfPlay);
-    fscanf(fp, "%d", &artist->amountOfSongs);
+    CHECK_RETURN_0_PRINT(artist->aboutMe,"Error reading artist CV\n")
+    if(fscanf(fp, "%d", &artist->amountOfPlay) != 1)
+    {
+        printf("Error reading artist's amount of played\n");
+        free(artist->name);
+        free(artist->aboutMe);
+        return 0;
+    }
+    if(fscanf(fp, "%d", &artist->amountOfSongs) != 1)
+    {
+        printf("Error reading artist's amount of songs\n");
+        free(artist->name);
+        free(artist->aboutMe);
+        return 0;
+    }
 
     return 1;
 }
@@ -97,9 +110,9 @@ int readArtistFromBFile(Artist * pArtist,FILE* fp)
     return 1;
 }
 
-void freeArtist(Artist *artist) {
+void freeArtist(Artist *artist)
+{
     free(artist->name);
     free(artist->aboutMe);
-//    artist->name = NULL;
-//    artist->aboutMe = NULL;
+
 }
