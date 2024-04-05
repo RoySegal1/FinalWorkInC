ad7a7405b3f788928fde1f0d10224fad1436651e
eilonaharoni <eaharoni88@gmail.com>
2024-04-05 20:08:46 +0300

    Merge remote-tracking branch 'origin/master' into Eilon_Branch

--- artistRepository.c
+++ artistRepository.c
@@ -14,7 +14,7 @@ int initArtistRepositoryFromFile(ArtistRepository* artistRepository,const char* 
     if (typeFile == FROM_TEXT_FILE)// init from text file
     {
         fp = fopen(fileName, "r");
-        CHECK_RETURN_0(fp)
+        CHECK_RETURN_0_PRINT(fp,"Error open artist file\n")
             if (fscanf(fp, "%d", &artistRepository->numOfArtist) != 1) {
                 fclose(fp);
                 return 0;
@@ -77,6 +77,51 @@ int initArtistRepositoryFromFile(ArtistRepository* artistRepository,const char* 
         return 1;
    }
     else //invalid input
+        return 0;
+}
+
+int saveArtistRepositoryFromFile(ArtistRepository* artistRepository,const char* fileName,int typeFile)
+{
+    FILE* fp;
+    if (typeFile == FROM_TEXT_FILE)// init from text file
+    {
+        fp = fopen(fileName, "w");
+        CHECK_RETURN_0_PRINT(fp,"Error open artist repository file to write\n")
+
+       if(!fprintf(fp, "%d\n", artistRepository->numOfArtist))
+       {
+           fclose(fp);
+           return 0;
+       }
+        for (int i = 0; i <artistRepository->numOfArtist ; ++i)
+        {
+           if(!saveArtistToTextFile(&artistRepository->allArtists[i],fp))
+               return 0;
+        }
+        return 1;
+    }
+    if (typeFile == FROM_BINARY_FILE)
+    {
+        fp = fopen(fileName, "wb");
+        CHECK_RETURN_0_PRINT(fp,"Error open artist repository file to write\n")
+
+        BYTE bCounter = (BYTE)artistRepository->numOfArtist;
+        if (fwrite(&bCounter, sizeof(BYTE),1,fp) != 1)
+        {
+            fclose(fp);
+            return 0;
+        }
+        for (int i = 0; i < artistRepository->numOfArtist; ++i)
+        {
+            if(!saveArtistToCompressFile(&artistRepository->numOfArtist[i],fp))
+            {
+                fclose(fp);
+                return 0;
+            }
+        }
+        return 1;
+
+    }else
         return 0;
 }
 
